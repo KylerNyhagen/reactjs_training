c85d424647d0ebb6e5bb04ef5655ff30986cabf6
Kyler Nyhagen <Kylersn5@gmail.com>
2018-12-05 19:58:19 -0600

    Testing out dynamic data.
    Investigation showed that these can only be put on one line, as per the video, however they can be done one after the other. Without a <br/>, they are put on the same line. It would be wise to put these in to HTML Tags if not using variables.

--- src/app/components/Home.jsx
+++ src/app/components/Home.jsx
@@ -2,9 +2,17 @@ import React from 'react'
 
 export class Home extends React.Component{
     render(){
+        let content = "";
+        if(true) {
+            content = <p>Hello!</p>
+        }
         return(
         <div>
             <p>In a new Component!</p>
+            { content }
+            { "Hello as a string not within a variable! It seems as those variables get put on their own lines...?\n"}
+            { 2+2 }
+            { 5 == 2 ? "Ternaries don't work" : "Ternaries work!"}
         </div>);
     }
 }
